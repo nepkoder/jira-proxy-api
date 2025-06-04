8451c3f9f9a1077d3dd918e963a412850e3f734a
Sujan Shrestha <sujans@gmeremit.com>
2025-06-04 17:08:07 +0900

    DONE

--- api/jira.js
+++ api/jira.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   };
   const today = new Date();
 
-  const projectKey =  "OT";
+  const projectKey = operation == "true" ? "OT" : "GMEBiz";
 
   // Build JQL
   let jql = `project=${projectKey}`;
