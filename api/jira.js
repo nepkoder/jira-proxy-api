2893f59cb9de4a06a67dbafae2f56d3f944df7c0
Sujan Shrestha <sujans@gmeremit.com>
2025-06-04 17:06:28 +0900

    udpate ot

--- api/jira.js
+++ api/jira.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   };
   const today = new Date();
 
-  const projectKey =  "Operation Team";
+  const projectKey =  "OT";
 
   // Build JQL
   let jql = `project=${projectKey}`;
